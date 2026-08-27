f0e6972e1f1854d4a965268564247c8bd49af119
Mattia Cavalieri Manasse <cavalierimanasse.2010869@strudenti.uniroma1.it>
2026-08-27 09:08:12 +0200

    added TakeScreenshot method

--- src/test/java/testingFramework/SubmitOrderTest.java
+++ src/test/java/testingFramework/SubmitOrderTest.java
@@ -1,9 +1,13 @@
 package testingFramework;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -91,11 +95,19 @@ public class SubmitOrderTest extends BaseTest {
 //		dataSet2.put("email", "test@test.it");
 //		dataSet2.put("password", "Test@000");
 //		dataSet2.put("product", "ADIDAS ORIGINAL");
-		
-		List<HashMap<String, String>> data = getJsonDataToMap(System.getProperty("user.dir") + "/src/test/java/data/purchaseOrder.json");
 
-		return new Object[][] { { data.get(0) }, { data.get(1) } };
+		List<HashMap<String, String>> data = getJsonDataToMap(
+				System.getProperty("user.dir") + "/src/test/java/data/purchaseOrder.json");
+
+		return new Object[][] { { data.get(0) }, { data.get(1)} };
 	}
-	
+
+	public String getScreenshot(String testCaseName) throws IOException {
+		TakesScreenshot ts = (TakesScreenshot) driver;
+		File source = ts.getScreenshotAs(OutputType.FILE);
+		File file = new File(System.getProperty("user.dir") + "/reports/" + testCaseName + ".png");
+		FileUtils.copyFile(source, file);
+		return System.getProperty("user.dir") + "/reports/" + testCaseName + ".png";
+	}
 
 }
